d0d18e917aadab873b60c6a4d9cf11e657d6d283
xodlf8044 <xodlf8044@gmail.com>
2020-12-03 04:55:17 +0900

    React API (After useReducer Before Hooks)

--- src/Users.js
+++ src/Users.js
@@ -1,32 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
-function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`unhandled action type: ${action.type}`);
+  }
+}
 
-  // async로 함수를 실행할 경우 해당 함수는 await가 끝나기 전까지 다음걸 실행하지 않는다.
+function Users() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' });
     try {
-      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setError(null);
-      setUsers(null);
-      // loading 상태를 true 로 바꿉니다.
-      setLoading(true);
-      // axios는 rest api를 사용하는 라이브러리로써 axios.get을 통해 다음에 들어갈 url에 들어간 정보( 정보는 무슨 형태?? maybe json? )를 response에 저장해준다.
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users'
       );
-      setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
